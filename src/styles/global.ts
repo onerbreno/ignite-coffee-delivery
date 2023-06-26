15208326e10109e6ac961524b9ce964ebd229889
Breno <onerbreno@outlook.com>
2023-06-26 20:11:31 -0300

    feat Add default theme variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,15 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    -webkit-font-smoothin: antialiased;
+  }
+
+  body, input, textarea, button {
+    background: ${(props) => props.theme['gray-100']};
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 `
---
+++ src/styles/themes/dafault.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-700': '#C47F17',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+
+  'stone-900': '#272221',
+  'stone-700': '#403937',
+  'stone-600': '#574F4D',
+  'stone-500': '#8D8686',
+  'stone-100': '#D7D5D5',
+
+  'gray-400': '#E6E5E5',
+  'gray-300': '#EDEDED',
+  'gray-200': '#F3F2F2',
+  'gray-100': '#FAFAFA',
+
+  white: '#fff',
+}

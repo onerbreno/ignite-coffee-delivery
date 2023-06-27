58578205966e09483e930d2a64a9a8798928f2c7
Breno <onerbreno@outlook.com>
2023-06-27 11:34:54 -0300

    refactor Rearrange color and font variables

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,62 @@
+export const defaultTheme = {
+  colors: {
+    brand: {
+      purple: {
+        dark: '#4B2995',
+        normal: '#8047F8',
+        light: '#EBE5F9',
+      },
+      yellow: {
+        dark: '#C47F17',
+        normal: '#DBAC2C',
+        light: '#F1E9C9',
+      },
+    },
+    base: {
+      white: '#FFFFFF',
+      background: '#FAFAFA',
+      card: '#F3F2F2',
+      input: '#EDEDED',
+      button: '#E6E5E5',
+      hover: '#D7D5D5',
+      label: '#8D8686',
+      text: '#574F4D',
+      subtitle: '#403937',
+      title: '#272221',
+    },
+  },
+  fonts: {
+    family: {
+      text: '"Roboto", sans-serif',
+      header: '"Baloo 2", cursive',
+    },
+    title: {
+      xl: '800 normal 3rem/1.3 "Baloo 2", cursive',
+      lg: '800 normal 2rem/1.3 "Baloo 2", cursive',
+      md: '800 normal 1.5rem/1.3 "Baloo 2", cursive',
+      sm: 'bold normal 1.25rem/1.3 "Baloo 2", cursive',
+      xs: 'bold normal 1.125rem/1.3 "Baloo 2", cursive',
+    },
+    text: {
+      lg: {
+        regular: '400 normal 1.25rem/1.3 "Roboto", sans-serif',
+        bold: 'bold normal 1.25rem/1.3 "Roboto", sans-serif',
+      },
+      md: {
+        regular: '400 normal 1rem/1.3 "Roboto", sans-serif',
+        bold: 'bold normal 1rem/1.3 "Roboto", sans-serif',
+      },
+      sm: {
+        regular: '400 normal 0.875rem/1.3 "Roboto", sans-serif',
+        bold: 'bold normal 0.75rem/1.3 "Roboto", sans-serif',
+      },
+    },
+    components: {
+      tag: 'bold normal 0.75rem/1.3 "Roboto", sans-serif',
+      button: {
+        lg: 'bold normal 0.875rem/1.6 "Roboto", sans-serif',
+        sm: 'bold normal 0.75rem/1.6 "Roboto", sans-serif',
+      },
+    },
+  },
+}
